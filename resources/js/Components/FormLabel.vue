71dfe978fe52e76cd46a525cbb22107ce0a1044e
pguerrero-at-wiris <pablo@wiris.com>
2024-08-07 12:56:59 +0200

    add label component and fix spacing

---
+++ resources/js/Components/FormLabel.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="d-flex align-center w-100 my-2">
+        <div class="text-h5 me-1">{{name}}</div>
+        <v-icon v-if="info" icon="mdi-information-outline" class="text-disabled"></v-icon>
+        <div v-if="required" class="ms-auto align-self-end text-subtitle-1 text-disabled font-italic">(required)</div>
+    </div>
+    
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+    name: string
+    required?: boolean
+    info?: string
+}>()
+</script>
+
+<style scoped>
+</style>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -3,38 +3,42 @@
         <v-card flat>
             <v-card-title class="text-h2">Bugolog</v-card-title>
         </v-card>
-        <form @submit.prevent="data.post('/send')">
+        <form @submit.prevent="data.post('/send')">         
             <v-container>
                 <v-row>
                     <v-col>
-                        <v-text-field variant="outlined" v-model="data.name" label="Name"></v-text-field>
+                        <FormLabel :name="'Name'" required/>
+                        <v-text-field variant="outlined" v-model="data.name" placeholder="John Doe"></v-text-field>
                     </v-col>
                 </v-row>
                 <v-row>
                     <v-col cols="12" md="6">
-                        <v-text-field variant="outlined" v-model="data.email" label="Email"></v-text-field>
+                        <FormLabel :name="'Email'" required />
+                        <v-text-field variant="outlined" v-model="data.email" placeholder="john.doe@example.com"></v-text-field>
                     </v-col>
-                    <v-col cols="12" sm="6">
-                        <v-text-field variant="outlined" v-model="data.position" label="Position"></v-text-field>
+                    <v-col cols="12" md="6">
+                        <FormLabel :name="'Product'" />
+                        <v-text-field variant="outlined" v-model="data.product" placeholder="Product"></v-text-field>
                     </v-col>
                 </v-row>
-                <v-row class="mb-8">
+                <v-row class="mt-6 mb-8">
                     <div class="h-100 px-3">
-                        <div class="text-h5">Issue Description</div>
-                        <div class="mt-2 mb-16" style="height: 300px;">
-                            <QuillEditor theme="snow" toolbar="full" v-model="data.issueDescription" />
+                        <FormLabel :name="'Issue Description'" required/>
+                        <div class="mt-4 mb-16" style="height: 300px;">
+                            <QuillEditor theme="snow" toolbar="full" v-model:content="data.issueDescription" />
                         </div>
                     </div>
                 </v-row>
                 <v-row class="">
-                    <v-col cols="12">
+                    <v-col cols="12" md="6">
+                        <FormLabel :name="'Attachments'"/>
                         <v-file-input variant="outlined" v-model="data.image" label="Attachments" prepend-icon=""
                             append-inner-icon="mdi-paperclip" multiple></v-file-input>
                     </v-col>
                 </v-row>
                 <v-row>
-                    <v-col cols="12">
-                        <v-btn type="submit" color="primary">Submit</v-btn>
+                    <v-col cols="12" class="d-flex justify-center justify-md-end">
+                        <v-btn type="submit" color="primary" size="large">Submit</v-btn>
                     </v-col>
                 </v-row>
             </v-container>
@@ -43,18 +47,17 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue';
-import { router, useForm } from '@inertiajs/vue3'
-
+import { useForm } from '@inertiajs/vue3'
 import { QuillEditor } from '@vueup/vue-quill'
 import '@vueup/vue-quill/dist/vue-quill.snow.css';
+import FormLabel from '@/Components/FormLabel.vue'
 
 const data = useForm({
     name: '',
     email: '',
     issueDescription: '',
     image: null,
-    position: ''
+    product: ''
 });
 
 </script>
