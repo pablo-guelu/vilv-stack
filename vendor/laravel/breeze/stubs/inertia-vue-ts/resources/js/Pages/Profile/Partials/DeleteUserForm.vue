b95b5f6c99826bf11426a79fa61eac47654bc0f5
pablo-guelu <pablo.guelu@gmail.com>
2024-09-30 01:40:47 +0200

    email support wip configuring resend package

--- vendor/laravel/breeze/stubs/inertia-vue-ts/resources/js/Pages/Profile/Partials/DeleteUserForm.vue
+++ vendor/laravel/breeze/stubs/inertia-vue-ts/resources/js/Pages/Profile/Partials/DeleteUserForm.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import v-btn from '@/Components/v-btn.vue';
+import DangerButton from '@/Components/DangerButton.vue';
 import InputError from '@/Components/InputError.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import Modal from '@/Components/Modal.vue';
@@ -35,6 +35,7 @@ const deleteUser = () => {
 const closeModal = () => {
     confirmingUserDeletion.value = false;
 
+    form.clearErrors();
     form.reset();
 };
 </script>
@@ -42,29 +43,39 @@ const closeModal = () => {
 <template>
     <section class="space-y-6">
         <header>
-            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">Delete Account</h2>
+            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                Delete Account
+            </h2>
 
             <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                Once your account is deleted, all of its resources and data will be permanently deleted. Before deleting
-                your account, please download any data or information that you wish to retain.
+                Once your account is deleted, all of its resources and data will
+                be permanently deleted. Before deleting your account, please
+                download any data or information that you wish to retain.
             </p>
         </header>
 
-        <v-btn @click="confirmUserDeletion">Delete Account</v-btn>
+        <DangerButton @click="confirmUserDeletion">Delete Account</DangerButton>
 
         <Modal :show="confirmingUserDeletion" @close="closeModal">
             <div class="p-6">
-                <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                <h2
+                    class="text-lg font-medium text-gray-900 dark:text-gray-100"
+                >
                     Are you sure you want to delete your account?
                 </h2>
 
                 <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-                    Once your account is deleted, all of its resources and data will be permanently deleted. Please
-                    enter your password to confirm you would like to permanently delete your account.
+                    Once your account is deleted, all of its resources and data
+                    will be permanently deleted. Please enter your password to
+                    confirm you would like to permanently delete your account.
                 </p>
 
                 <div class="mt-6">
-                    <InputLabel for="password" value="Password" class="sr-only" />
+                    <InputLabel
+                        for="password"
+                        value="Password"
+                        class="sr-only"
+                    />
 
                     <TextInput
                         id="password"
@@ -80,16 +91,18 @@ const closeModal = () => {
                 </div>
 
                 <div class="mt-6 flex justify-end">
-                    <SecondaryButton @click="closeModal"> Cancel </SecondaryButton>
+                    <SecondaryButton @click="closeModal">
+                        Cancel
+                    </SecondaryButton>
 
-                    <v-btn
+                    <DangerButton
                         class="ms-3"
                         :class="{ 'opacity-25': form.processing }"
                         :disabled="form.processing"
                         @click="deleteUser"
                     >
                         Delete Account
-                    </v-btn>
+                    </DangerButton>
                 </div>
             </div>
         </Modal>
